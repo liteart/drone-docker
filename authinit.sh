143fe66510eff7654afc0b43ad826b471bef6063
Simon Hugentobler <simon.hugentobler@bertschi.com>
2017-03-16 10:38:35 +0100

    updated script

--- authinit.sh
+++ authinit.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+echo "authInit is running"
+
 if [ -n "$PRIVATE_REPO_PULL_USER" -a -n "$PRIVATE_REPO_PULL_PASSWORD" -a -n "$PRIVATE_REPO_PULL_ADDRESS" ]; then
     echo "logging in to private docker registry..."
     docker login --username="$PRIVATE_REPO_PULL_USER" --password="$PRIVATE_REPO_PULL_PASSWORD" $PRIVATE_REPO_PULL_ADDRESS
 fi
 
-./usr/local/bin/dockerd-entrypoint.sh $@
+exec /usr/local/bin/dockerd-entrypoint.sh "$@"
